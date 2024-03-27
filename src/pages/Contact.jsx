9ee5ad59582eb32297fd4a64b41b08a1d0730c9a
Sourav Dutta <souravduttasd2@gmail.com>
2024-03-27 17:53:25 +0530

    add loading in contact page submit btn

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -10,36 +10,35 @@ function Contact() {
 
   const onSubmitHandle = async (e) => {
     e.preventDefault();
-    setIsLoading(true)
+    setIsLoading(true);
     const data = {
       name,
       email,
       message,
     };
 
-    try{
-    const {status} = await axios.post(import.meta.env.VITE_APP_URL, data);
+    try {
+      const { status } = await axios.post(import.meta.env.VITE_APP_URL, data);
 
-    if (status === 200) {
-      setName("");
-      setEmail("");
-      setMessage("");
-      setIsLoading(false);
+      if (status === 200) {
+        setName("");
+        setEmail("");
+        setMessage("");
+        setIsLoading(false);
+        Swal.fire(
+          "Thank You!",
+          "I will try to answer as soon as possible.",
+          "success"
+        );
+      }
+    } catch (error) {
+      console.error("Error is - " + error);
       Swal.fire(
-        'Thank You!',
-        'I will try to answer as soon as possible.',
-        'success'
-      )
-    } 
-  }
-  catch(error){
-    console.error("Error is - "+error);
-    Swal.fire(
-      'Error',
-      'An error occurred while processing your request. Please try again later.',
-      'error'
-    );
-  };
+        "Error",
+        "An error occurred while processing your request. Please try again later.",
+        "error"
+      );
+    }
   };
   return (
     <div>
@@ -65,13 +64,12 @@ function Contact() {
                 <h2 className="title-font font-bold font-serif tracking-widest">
                   EMAIL
                 </h2>
-                <a href="mailto:hello@dsourav.com" className="text-indigo-500 leading-relaxed">
+                <a
+                  href="mailto:hello@dsourav.com"
+                  className="text-indigo-500 leading-relaxed"
+                >
                   hello@dsourav.com
                 </a>
-                {/* <h2 className="title-font font-bold font-serif tracking-widest mt-4">
-                  PHONE
-                </h2> */}
-                {/* <p className="leading-relaxed">+91 9641804633</p> */}
               </div>
             </div>
           </div>
@@ -125,36 +123,37 @@ function Contact() {
                 ></textarea>
               </div>
               {isLoading ? (
-            <button
-              type="button"
-              className="px-4 py-2 bg-indigo-500 text-white border rounded-md"
-              disabled
-            >
-              <svg
-                aria-hidden="true"
-                className="inline w-6 h-8 mr-3 text-gray-200 animate-spin dark:text-gray-600 fill-gray-600 dark:fill-gray-300"
-                viewBox="0 0 100 101"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-                  fill="currentColor"
-                />
-                <path
-                  d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-                  fill="currentFill"
-                />
-              </svg>
-              Sending...
-            </button>
-          ) :(
-              <button
-                type="submit"
-                className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg"
-              >
-                Send
-              </button>)}
+                <button
+                  type="button"
+                  className="px-4 py-2 bg-indigo-500 text-white border rounded-md"
+                  disabled
+                >
+                  <svg
+                    aria-hidden="true"
+                    className="inline w-6 h-8 mr-3 text-gray-200 animate-spin dark:text-gray-600 fill-gray-600 dark:fill-gray-300"
+                    viewBox="0 0 100 101"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                      fill="currentColor"
+                    />
+                    <path
+                      d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                      fill="currentFill"
+                    />
+                  </svg>
+                  Sending...
+                </button>
+              ) : (
+                <button
+                  type="submit"
+                  className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg"
+                >
+                  Send
+                </button>
+              )}
             </form>
           </div>
         </div>
